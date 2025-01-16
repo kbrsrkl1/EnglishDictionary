70303da97b334f638ead235c1426a1aa4708317f
kbrsrkl1 <kubranur216@gmail.com>
2025-01-16 19:27:22 +0300

    commit

--- app.js
+++ app.js
@@ -13,13 +13,23 @@ async function fetchAPI(word){
             infoTextEl.innerText = `Searching the meaning of "${word}"`
             const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
             const result =await fetch(url).then((res) => res.json());
+
+            if(result.title){
+                meaningContainerEl.style.display = "block"
+                titleEl.innerText = word
+                meaningEl.innerText = "N/A"
+                audioEl.style.display = "none"
+            }else{ 
             infoTextEl.style.display = "none";
             meaningContainerEl.style.display = "block";
+            audio.style.display =  "inline-flex"
             titleEl.innerText = result[0].word
             meaningEl.innerText = result[0].meanings[0].definitions[0].definition
             audioEl.src = result[0].phonetics[0].audio
+            }
         } catch (error) {
             console.log(error)
+            infoTextEl.innerText = `An error happened, try again later`
         }
 }
 
